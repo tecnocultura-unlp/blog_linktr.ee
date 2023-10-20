4851b81928f71223b8f1e96564d2919f88f0ad60
tecnocultura-unlp <148548966+tecnocultura-unlp@users.noreply.github.com>
2023-10-20 08:48:49 -0300

    Add files via upload
    linktree landpage

---
+++ index.js
@@ -0,0 +1,16 @@
+const shareButtons = document.querySelectorAll('.tile-share-button');
+
+async function copyText(e){
+    e.preventDefault();
+    const link = this.getAttribute('link');
+    console.log(link);
+    try {
+        await navigator.clipboard.writeText(link);
+        alert("Copied the text: " + link);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+shareButtons.forEach(shareButton => 
+    shareButton.addEventListener('click', copyText));
